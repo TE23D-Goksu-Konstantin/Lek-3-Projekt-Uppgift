81d7d3d5b33345d533c2819a84590c8c0c4001d0
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2025-09-02 13:06:45 +0200

    Began with visualising the seats/continued with the scanner

---
+++ src/App.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String[] seats = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"};
+        int[] seatsPersNr = {};
+        String[] seatsName = {};
+
+
+
+
+        System.out.println(
+            
+        "├"+seats[0]+"─"+seats[1]+"─ ─"+seats[2]+"─"+seats[3]+"┤\r\n" + 
+        "├"+seats[4]+"─"+seats[5]+"─ ─"+seats[6]+"─"+seats[7]+"┤\r\n" + 
+        "├"+seats[8]+"─"+seats[9]+"─ ─"+seats[10]+"─"+seats[11]+"┤\r\n" + //Chart for visuals
+        "├"+seats[12]+"─"+seats[13]+"─ ─"+seats[14]+"─"+seats[15]+"┤\r\n" + 
+        "├"+seats[16]+"─"+seats[17]+"─ ─"+seats[18]+"─"+seats[19]+"┤\r\n");
+
+        System.out.println("Hello person, do you wish to, \n1. Book\n2. Cancel\na seat.");
+        Scanner keyB = new Scanner(System.in);
+        String bookChoice = keyB()
+        if()
+
+
+
+    }
+
+
+
+
+
+
+
+
+    public static int ID()
+    {
+
+    }
+    // please give me your Personnummer by this given formula (YYMMDD) 
+}
